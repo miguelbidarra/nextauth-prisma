a47152efcab26dde9840322bc248e2781fdb939c
miguelbidarra <miguelbidarrab@gmail.com>
2024-10-01 23:12:21 +0100

    whopa

--- src/app/apiFromServer/page.tsx
+++ src/app/apiFromServer/page.tsx
@@ -3,7 +3,6 @@ import { headers } from "next/headers";
 export default async function APIFromServer() {
   const resp = await fetch("http://localhost:3000/api/whoAmI", {
     method: "GET",
-    headers: headers(),
   }).then((res) => res.json());
 
   return (
